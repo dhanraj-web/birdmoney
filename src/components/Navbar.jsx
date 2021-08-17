42fc368f10e65a3508ff96e99b3e2094708fa8ef
dhanraj-web <dhanraj@aequalisys.com>
2021-08-17 13:05:34 +0530

    scss file names updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,12 @@ import React from "react";
 const Navbar = () => {
   return (
     <nav className="nav-bar">
-      <div className="main-logo">LOGO</div>
+      <div className="main-logo">
+        <img
+          src="https://www.bird.money/static/bird-new/images/logo.svg"
+          alt=""
+        />
+      </div>
       <ul className="nav-container">
         <li>Flight path</li>
         <li>Use Case</li>
